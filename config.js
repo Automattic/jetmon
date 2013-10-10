6064795f0f719321175a1f5148ff26e3829846a3
Miguel Lezama <miguel@libre.coop>
2013-10-10 11:18:27 -0200

    Add bucket_no to query Add config file

---
+++ config.js
@@ -0,0 +1,18 @@
+var config = {};
+
+config.mysql = {};
+config.mysql.host     = 'localhost';
+config.mysql.user     = '';
+config.mysql.password = '',
+config.mysql.database = 'jetmon',
+
+config.bucket_no           = 1;
+config.numWorkers          = 200; // Number of Workers to create
+config.NUM_TO_PROCESS      = 10; // Number of simultaneous connections per worker
+
+config.NUM_OF_CHECKS       = 4;
+config.TIME_BETWEEN_CHECKS = 20; //seconds
+
+config.HTTP_PORT           = 80;
+
+module.exports = config;
