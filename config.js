c6ffea6de350bf3237ea5461c9e8cb3261e32093
Miguel Lezama <miguel@libre.coop>
2013-10-15 13:39:22 -0200

    Re-run the monitor on config.MONITOR_INTERVAL basis. Create needed workers only.

--- config.js
+++ config.js
@@ -11,8 +11,9 @@ config.numWorkers          = 200; // Number of Workers to create
 config.NUM_TO_PROCESS      = 10; // Number of simultaneous connections per worker
 
 config.NUM_OF_CHECKS       = 4;
-config.TIME_BETWEEN_CHECKS = 20; //seconds
+config.TIME_BETWEEN_CHECKS = 20; // Seconds before rechecking a site
 
+config.MONITOR_INTERVAL    = 10000; // Milliseconds between monitor runs
 config.HTTP_PORT           = 80;
 
 config.sendmails           = false;
