55cb4a5546dc788be2e02012a78859668a528f7d
Miguel Lezama <miguel@libre.coop>
2013-10-16 06:15:27 -0200

    Revert 8abf9a4..31eb52e
    This rolls back to commit 8abf9a404fe44952c277ac81dc397302ae8940f3.

--- config.js
+++ config.js
@@ -7,16 +7,12 @@ config.mysql.password = '',
 config.mysql.database = 'jetmon',
 
 config.bucket_no           = 1;
-config.bucket_no_min       = 0
-config.bucket_no_max       = 512;
-
 config.numWorkers          = 200; // Number of Workers to create
 config.NUM_TO_PROCESS      = 10; // Number of simultaneous connections per worker
 
 config.NUM_OF_CHECKS       = 4;
-config.TIME_BETWEEN_CHECKS = 20; // Seconds before rechecking a site
+config.TIME_BETWEEN_CHECKS = 20; //seconds
 
-config.MONITOR_INTERVAL    = 10000; // Milliseconds between monitor runs
 config.HTTP_PORT           = 80;
 
 config.sendmails           = false;
