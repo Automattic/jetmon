5bce8d38a5bcf9cb82b11a4660939c19d7175325
David Newman <dnewman@automattic.com>
2015-05-11 10:23:55 +1000

    Support node v0.12.x branch compatibility
    Sweeping changes to change the affected native node module calls up to
    node version 0.12.x compatibility.

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include <unistd.h>
 
 #include <node.h>
+#include <uv.h>
 
 using namespace v8;
 using namespace node;
@@ -15,25 +16,25 @@ using namespace node;
 #include "http_checker.h"
 
 struct HTTP_Check_Baton {
-	Persistent<Function> callback;
+	CopyablePersistentTraits<Function>::CopyablePersistent callback;
 	HTTP_Checker *http_checker;
 	std::string server;
 	int port;
 	int server_id;
 };
 
-void http_check_async_fin( uv_work_t *req, int status ) {
-	HandleScope scope;
-	HTTP_Check_Baton *baton = static_cast<HTTP_Check_Baton*>(req->data);
-	Handle<Value> argv[3] = { Number::New( baton->server_id ),  Number::New( baton->http_checker->get_rtt() ),
-								Number::New( baton->http_checker->get_response_code() ) };
-	TryCatch try_catch;
-	MakeCallback( Context::GetCurrent()->Global(), baton->callback, 3, argv );
-	if ( try_catch.HasCaught() )
-		FatalException( try_catch );
+static void http_check_async_fin( uv_work_t *req, int status ) {
+	Isolate* isolate = Isolate::GetCurrent();
+	HandleScope scope( isolate );
 
-	baton->callback.Dispose();
-	baton->callback.Clear();
+	HTTP_Check_Baton *baton = static_cast<HTTP_Check_Baton*>(req->data);
+	Handle<Value> argv[3] = { Number::New( isolate, baton->server_id ),
+								Number::New( isolate, baton->http_checker->get_rtt() ),
+								Number::New( isolate, baton->http_checker->get_response_code() ) };
+
+	Local<Function> cb_func = Local<Function>::New( isolate, baton->callback );
+	cb_func->Call( isolate->GetCurrentContext()->Global(), 3, argv );
+	baton->callback.Reset();
 	delete baton->http_checker;
 	delete baton;
 	delete req;
@@ -44,56 +45,60 @@ void http_check_async( uv_work_t *req ) {
 	baton->http_checker->check( baton->server, baton->port );
 }
 
-Handle<Value> http_check( const Arguments &args ) {
-	HandleScope scope;
+void http_check( const FunctionCallbackInfo<Value>& args ) {
+	args.GetReturnValue().SetUndefined();
+	Isolate* isolate = args.GetIsolate();
+	HandleScope scope( isolate );
+
 	if ( args.Length() < 4 ) {
-		ThrowException( Exception::TypeError( String::New( "Wrong number of arguments" ) ) );
-		return scope.Close( Undefined() );
+		isolate->ThrowException( Exception::TypeError(
+			String::NewFromUtf8( isolate, "Wrong number of arguments" ) ) );
+		return;
 	}
 
 	if ( ! args[1]->IsNumber() ) {
-		ThrowException( Exception::TypeError( String::New( "The port number argument is not valid" ) ) );
-		return scope.Close( Undefined() );
+		isolate->ThrowException( Exception::TypeError(
+			String::NewFromUtf8( isolate, "The port number argument is not valid" ) ) );
+		return;
 	}
 
 	if ( ! args[2]->IsNumber() ) {
-		ThrowException( Exception::TypeError( String::New( "The server id argument is not valid" ) ) );
-		return scope.Close( Undefined() );
+		isolate->ThrowException( Exception::TypeError(
+			String::NewFromUtf8( isolate, "The server id argument is not valid" ) ) );
+		return;
 	}
 
 	if ( ! args[3]->IsFunction() ) {
-		ThrowException( Exception::TypeError( String::New( "You have not provided a callback function as the 4th parameter" ) ) );
-		return scope.Close( Undefined() );
+		isolate->ThrowException( Exception::TypeError(
+			String::NewFromUtf8( isolate, "You have not provided a callback function as the 4th parameter" ) ) );
+		return;
 	}
 
 	HTTP_Check_Baton *baton = new HTTP_Check_Baton();
 	HTTP_Checker *checker = new HTTP_Checker();
 	baton->http_checker = checker;
 
-	String::AsciiValue sHost( args[0]->ToString() );
+	String::Utf8Value sHost( args[0]->ToString() );
 	baton->server = *sHost;
 
 	baton->port = args[1]->NumberValue();
 	baton->server_id = args[2]->NumberValue();
 
-	baton->callback = Persistent<Function>::New( args[3].As<Function>() );
+	CopyablePersistentTraits<Function>::CopyablePersistent percy( isolate, args[3].As<Function>() );
+	baton->callback.Reset( isolate, percy );
 
 	uv_work_t *req = new uv_work_t();
 	req->data = baton;
 
 	uv_queue_work( uv_default_loop(), req, http_check_async, (uv_after_work_cb)http_check_async_fin );
-
-	return scope.Close( Undefined() );
 }
 
 void Initialise( Handle<Object> exports) {
-	HandleScope scope;
-
 	SSL_load_error_strings();
 	SSL_library_init();
 	OpenSSL_add_all_algorithms();
 
-	exports->Set( String::NewSymbol( "http_check" ), FunctionTemplate::New( http_check )->GetFunction() );
+	NODE_SET_METHOD( exports, "http_check", http_check );
 }
 
 NODE_MODULE( jetmon, Initialise )
