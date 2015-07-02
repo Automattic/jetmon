172b7de6234cac7d23ef51655df72ee83af01abb
David Newman <dnewman@automattic.com>
2015-07-02 12:31:26 +1000

    Remove debug code.

--- veriflier/source/main.cpp
+++ veriflier/source/main.cpp
@@ -1,6 +1,5 @@
 
 #include <QCoreApplication>
-#include <QUrl>
 
 #include "headers/config.h"
 #include "headers/logger.h"
@@ -12,11 +11,6 @@ int main( int argc, char *argv[] )
 {
 	QCoreApplication app(argc, argv);
 	Logger::instance()->startLogger();
-
-
-	QUrl bob( "http://xn--80a3b8a.xn--p1ai/" );
-
-	qDebug() << bob.host();
 
 	SSL_Server *ssl = new SSL_Server();
 	bool result = ssl->listen( QHostAddress::Any, Config::instance()->get_int_value( "listen_port" ) );
