de900db076dda341fe654c33eba2477931c47474
David Newman <dnewman@automattic.com>
2017-01-10 12:57:03 +1000

    Read the max_checkers from the config file
    * Add some error checking for the SSL settings.
    * Change wording in the logs for the thread pool setting.

--- veriflier/source/ssl_server.cpp
+++ veriflier/source/ssl_server.cpp
@@ -10,7 +10,7 @@ SSL_Server::SSL_Server( QObject *parent ) : QTcpServer( parent ) {
 
 	pool = new QThreadPool(this);
 	pool->setMaxThreadCount( Config::instance()->get_int_value( "thread_pool_max" ) );
-	LOG( ( QString( "comms max threads: " ) + QString::number( pool->maxThreadCount() ) ) );
+	LOG( ( QString( "comms thread pool: " ) + QString::number( pool->maxThreadCount() ) ) );
 
 	this->setMaxPendingConnections( Config::instance()->get_int_value( "max_pending_conns" ) );
 	LOG( ( QString( "max pending conns: " ) + QString::number( this->maxPendingConnections() ) ) );
@@ -20,6 +20,10 @@ SSL_Server::SSL_Server( QObject *parent ) : QTcpServer( parent ) {
 	m_net_timeout = Config::instance()->get_int_value( "net_comms_timeout" );
 	m_debug = Config::instance()->get_bool_value( "debug" );
 	m_jetmon_server_port = Config::instance()->get_int_value( "jetmon_server_port" );
+
+	int max_checkers = Config::instance()->get_int_value( "max_checkers" );
+	if ( 0 == max_checkers || -1 == max_checkers ) max_checkers = DEFAULT_MAX_CHECKERS;
+	LOG( ( QString( "max checkers: " ) + QString::number( max_checkers ) ) );
 
 	int max_checks = Config::instance()->get_int_value( "max_checks" );
 	if ( 0 == max_checks || -1 == max_checks ) max_checks = DEFAULT_MAX_CHECKS;
@@ -31,17 +35,27 @@ SSL_Server::SSL_Server( QObject *parent ) : QTcpServer( parent ) {
 
 	QFile keyFile( Config::instance()->get_string_value( "privatekey_file" ) );
 	keyFile.open( QFile::ReadOnly );
+	if ( ! keyFile.isOpen() ) {
+		LOG( "Error loading private key file" );
+		cerr << "Error loading private key file" << endl;
+		exit(-1);
+	}
 	QSslKey ssl_key( &keyFile, QSsl::Rsa );
 	m_ssl_config->setPrivateKey( ssl_key );
 	keyFile.close();
 
 	QFile certFile( Config::instance()->get_string_value( "privatecert_file" ) );
 	certFile.open( QFile::ReadOnly );
+	if ( ! certFile.isOpen() ) {
+		LOG( "Error loading private certificate file" );
+		cerr << "Error loading private key file" << endl;
+		exit(-1);
+	}
 	QSslCertificate ssl_cert( &certFile );
 	m_ssl_config->setLocalCertificate( ssl_cert );
 	certFile.close();
 
-	m_checker = new CheckController( m_ssl_config, m_jetmon_server_port, max_checks,
+	m_checker = new CheckController( m_ssl_config, m_jetmon_server_port, max_checkers, max_checks,
 									 m_veriflier_name, m_auth_token, m_net_timeout, m_debug );
 
 	connect( this, SIGNAL( acceptError(QAbstractSocket::SocketError) ), this, SLOT( logError(QAbstractSocket::SocketError) ) );
