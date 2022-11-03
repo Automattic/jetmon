e9cccc4bdcaee0e6628598f2ca2e34cc70bd7e4b
Foteini Giannaropoulou <giannaropoulou.foteini@gmail.com>
2022-11-03 10:47:07 +0200

    Docker: Properly handle config files and secrets

--- docker/run-veriflier.sh
+++ docker/run-veriflier.sh
@@ -10,4 +10,6 @@ if [ ! -f certs/veriflier.key ] && [ ! -f certs/veriflier.crt ]; then
 	openssl req -newkey rsa:2048 -nodes -keyout certs/veriflier.key -x509 -days 365 -out certs/veriflier.crt -subj "/C=US/ST=California/L=San Francisco/O=Automattic Inc./CN=jetmon"
 fi
 
+sed -e "s/<JETMON_PORT>/${JETMON_PORT}/g" -e "s/<VERIFLIER_PORT>/${VERIFLIER_PORT}/g" -e "s/<VERIFLIER_AUTH_TOKEN>/${VERIFLIER_AUTH_TOKEN}/g" config/veriflier-sample.json > config/veriflier.json
+
 exec ./veriflier start
